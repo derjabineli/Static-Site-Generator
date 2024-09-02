574acbb08ba1ac5becc1dfa3aad8a8e01ddb6d94
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-09-02 13:13:16 -0700

    Add unit tests for text nodes

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,13 @@
+from htmlnode import LeafNode
+
 class TextNode:
+    text_type_text = "text"
+    text_type_bold = "bold"
+    text_type_italic = "italic"
+    text_type_code = "code"
+    text_type_link = "link"
+    text_type_image = "image"
+
     def __init__(self, text, text_type, url):
         self.text = text
         self.text_type = text_type
@@ -11,3 +20,20 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+    def text_node_to_html_node(self):
+         match (self.text_type):
+             case self.text_type_text:
+                 return LeafNode("", self.text)
+             case self.text_type_bold:
+                 return LeafNode("b", self.text)
+             case self.text_type_italic:
+                 return LeafNode("i", self.text)
+             case self.text_type_code:
+                 return LeafNode("code", self.text)
+             case self.text_type_link:
+                 return LeafNode("a", self.text, {"href": self.url})
+             case self.text_type_image:
+                return LeafNode("img", "", {"src": self.url, "alt": self.text})
+             case _:
+                 raise Exception("Text node is not a valid type")
