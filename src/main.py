d129e2841ddb9e65db2d9417b1321a63e3cc039e
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-09-10 23:26:42 -0700

    call page generator function from main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import os
 import shutil
 from copy_static import copy_files
+from generate_page import generate_page
 
 def main():
     if os.path.exists("public"):
         shutil.rmtree("public")
     copy_files("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
     
 
 main()
