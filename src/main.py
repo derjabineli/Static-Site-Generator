7d39e71115e129ba335f41e89b349e36396e4865
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-09-10 23:59:19 -0700

    call recursive html page generation function

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import os
 import shutil
 from copy_static import copy_files
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 
 def main():
     if os.path.exists("public"):
         shutil.rmtree("public")
     copy_files("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
     
 
 main()
